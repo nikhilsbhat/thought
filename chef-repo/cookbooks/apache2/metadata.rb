024d0da133a451a0253c72c69cd40ea023b8a97a
root <root@instance-1.us-east1-b.c.earnest-entropy-228417.internal>
2019-01-17 10:40:47 +0000

    adding apache2 | mediamysql | wikimedia_base cookbooks

--- chef-repo/cookbooks/apache2/metadata.rb
+++ chef-repo/cookbooks/apache2/metadata.rb
@@ -1,9 +1,9 @@
-name 'wikimedia_base'
+name 'apache2'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures wikimedia_base'
-long_description 'Installs/Configures wikimedia_base'
+description 'Installs/Configures apache2'
+long_description 'Installs/Configures apache2'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
@@ -11,13 +11,10 @@ chef_version '>= 12.14' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/wikimedia_base/issues'
+# issues_url 'https://github.com/<insert_org_here>/apache2/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/wikimedia_base'
-
-depends 'apache2'
-depends 'php'
+# source_url 'https://github.com/<insert_org_here>/apache2'
---
+++ chef-repo/cookbooks/mediamysql/attributes/default.rb
@@ -0,0 +1,4 @@
+# though these attributes are not directly used by this cookbook
+# this helps while setting up of wikimedia
+
+default["sql"]["type"] = 'mysql'
